3b581f82e98982a93666db28099071d2c5d8aad2
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:51:53 -0600

    Update Hotel2.cpp

--- Hotel2.cpp
+++ Hotel2.cpp
@@ -2,9 +2,7 @@
 #include <ctime>
 #include <iostream>
 #include "Reserva.cpp"
-#include "Habitacion.cpp"
-#include "Deluxe.cpp"
-#include "Simple.cpp"
+
 using namespace std;
 
 int main() {
@@ -26,6 +24,7 @@ int main() {
      while (continua == 1){
         cout<<"\n1. Continuar."<<endl;
         cout<<"2. Modificar datos."<<endl;
+        cout<<"3. Salir"<<endl;
         cout<<"Ingrese una opcion: "; cin>>opcion;
         system("cls");
         if (opcion == 1){
@@ -33,18 +32,33 @@ int main() {
             continua = 0;
         }
         else {
-            cout<<"Fecha de llegada: "; cin>>diaLlegada;
-            cout<<"Fecha de salida: "; cin>>diaSalida;
-            cout<<"Nro. de huespedes: "; cin>>nHuesped;
-            reserva1.setLlegada(diaLlegada);
-            reserva1.setSalida(diaSalida);
-            reserva1.setnHuesped(nHuesped);
-            cout<<"\nVerifique si los datos son correctos "<<endl;
-            cout<<endl<<reserva1.mostrarPeriodo()<<endl;
-            cout<<"\nNro. Huespedes: "<<reserva1.getnHuesped()<<endl;
+            if (opcion == 2){
+                cout<<"Fecha de llegada: "; cin>>diaLlegada;
+                cout<<"Fecha de salida: "; cin>>diaSalida;
+                cout<<"Nro. de huespedes: "; cin>>nHuesped;
+                reserva1.setLlegada(diaLlegada);
+                reserva1.setSalida(diaSalida);
+                reserva1.setnHuesped(nHuesped);
+                cout<<"\nVerifique si los datos son correctos "<<endl;
+                cout<<endl<<reserva1.mostrarPeriodo()<<endl;
+                cout<<"\nNro. Huespedes: "<<reserva1.getnHuesped()<<endl;
+            }
+            else {
+                if (opcion == 3){
+                    cout<<"RESERVA CANCELADA. HASTA LUEGO"<<endl;
+                    continua = 0;
+                }
+                else {
+                    cout<<"OPCION INVALIDA"<<endl;
+                    cout<<"\nVerifique si los datos son correctos "<<endl;
+                    cout<<endl<<reserva1.mostrarPeriodo()<<endl;
+                    cout<<"\nNro. Huespedes: "<<reserva1.getnHuesped()<<endl;
+                }
+            }
         }
      }
-    string nombre, apellido, nacionalidad; int edad; string correo, telefono;
+    if (opcion != 3){
+        string nombre, apellido, nacionalidad; int edad; string correo, telefono;
     cout<<"INGRESE DATOS HUESPED"<<endl;
     for (int i = 0; i < nHuesped; i++){
         cout<<"Huesped Nro. "<<i+1<<endl;
@@ -88,7 +102,7 @@ int main() {
         }
     }
     cout<<"HABITACIONES"<<endl;
-    int ndeluxe, nsimple; float precioTotal; string tecla;
+    int ndeluxe, nsimple; int num = 0; float precioTotal; string tecla;
     cout<<"Cuantas habitaciones Deluxe requiere: "; cin>>ndeluxe;
     cout<<"Cuantas habitaciones Simple requiere: "; cin>>nsimple;
     system("cls");
@@ -100,10 +114,10 @@ int main() {
             cout<<"Jacuzzi (1/0): "; cin>>jacuzzi;
             cout<<"Vista al Mar (1/0): "; cin>>vistaMar;
             cout<<"Comida (Buffet/Simple): "; cin>>comida;
-            Deluxe deluxe1(700.0, jacuzzi, vistaMar, comida);
-            cout<<"Precio total de la habitacion: $"<<deluxe1.calcularPrecioTotal()<<endl;
-            cout<<"Su numero de habitacion asignado es: "<<deluxe1.getNumHabitacion()<<endl;
-            precioTotal = precioTotal + deluxe1.calcularPrecioTotal();
+            Reserva reserva1(700.0, jacuzzi, vistaMar, comida);
+            cout<<"Precio total de la habitacion: $"<<reserva1.calcularPrecioDeluxe(700.0, jacuzzi, vistaMar, comida)<<endl;
+            cout<<"Su numero de habitacion asignado es: "<<reserva1.numHabitacionDeluxe()+ num<<endl;
+            precioTotal = precioTotal + reserva1.calcularPrecioDeluxe(700.0, jacuzzi, vistaMar, comida);
             cout<<"Presione cualquier tecla para continuar: "; cin>>tecla;
         }
     }
@@ -114,18 +128,18 @@ int main() {
         for (int i = 0; i < nsimple; i++){
             cout<<endl<<"Habitacion Nro. "<<i+1<<endl;
             cout<<"Hidromasaje (1/0): "; cin>>hidromasaje;
-            Simple simple1(500.0, hidromasaje);
-            cout<<"Precio total de la habitacion: $"<<simple1.calcularPrecio()<<endl;
-            cout<<"Su numero de habitacion asignado es: "<<simple1.getNumHabitacion()<<endl;
+            Reserva reserva1(500.0, hidromasaje);
+            cout<<"Precio total de la habitacion: $"<<reserva1.calcularPrecioSimple(500.0, hidromasaje)<<endl;
+            cout<<"Su numero de habitacion asignado es: "<<reserva1.numHabitacionSimple() + num<<endl;
             cout<<"Presione cualquier tecla para continuar: "; cin>>tecla;
-            precioTotal = precioTotal + simple1.calcularPrecio();
+            precioTotal = precioTotal + reserva1.calcularPrecioSimple(500.0, hidromasaje);
+            num = num + 1;
         }
     }
     system("cls");
     cout<<endl<<"PARA FINALIZAR POR FAVOR INTRODUZCA LOS DATOS DE CONTACTO"<<endl;
     cout<<"Correo electronico: "; cin>>correo;
     cout<<"Numero telefonico: "; cin>>telefono;
-    //Reserva reserva1(correo, telefono);
     system("cls");
     cout<<"DATOS DE CONTACTO"<<endl;
     Reserva reserva2(correo, telefono);
@@ -136,6 +150,6 @@ int main() {
     cout<<"Precio total: $"<<precioTotal<<endl;
     cout<<"CODIGO DE RESERVA: "<<reserva1.getCodigo()<<endl;
     cout<<"GRACIAS POR HABER CONFIADO EN NOSOTROS. LOS ESPERAMOS!!!"<<endl;
+    }
     return 0;
 }
-
