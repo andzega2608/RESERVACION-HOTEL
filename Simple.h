21dac714fa4d1a0fe7ea5a468083a21a4ed791de
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:50:38 -0600

    Update Simple.h

--- Simple.h
+++ Simple.h
@@ -1,10 +1,13 @@
+/*En comparación con la habitación Deluxe, la habitación Simple es un cuarto sencillo, el cual solo dispone de los elementos más
+básicos para que el huesped puede tener un descanso reparador. Sin embargo, es importante mencionar que también el cliente puede
+personalizar su haabitación con el tipo de ducha que desea, como ser hidromasaje o no.*/
 #include "Habitacion.h"
-
 class Simple : public Habitacion{
     private:
         bool hidromasaje;
     public:
         Simple();
         Simple(float, bool);
+        void setHidromasaje(bool);
         float calcularPrecio();
 };
