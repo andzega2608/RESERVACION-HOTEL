3661996f66f4d0c598fbf5aa1a0f6aa7e3e28214
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:49:44 -0600

    Update Reserva.h

--- Reserva.h
+++ Reserva.h
@@ -1,5 +1,12 @@
+/*La superclase de todo el programa, es la clase Reserva, es en esta clase que mediante la aplicacion de agregacion, composicion
+y herencia, el usuario puede ingresar gran parte de los datos, como ser el día de llegada, dia de salida, datos de contacto, así
+también como los datos de todos los huespedes.
+*/
 #include "Huesped.cpp"
 #include "Personal.cpp"
+#include "Habitacion.cpp"
+#include "Deluxe.cpp"
+#include "Simple.cpp"
 #include <string>
 using namespace std;
 
@@ -11,11 +18,15 @@ class Reserva{
         int nHuesped;
         Huesped datoHuesped;
         Personal contactoHuesped;
+        Deluxe habDeluxe;
+        Simple habSimple;
     public:
         Reserva();
         Reserva(string, string);
         Reserva(string, string, string, int);
         Reserva(string, string, string);
+        Reserva(float, bool, bool, string);
+        Reserva(float, bool);
         double getCodigo();
         void setLlegada(string);
         void setSalida(string);
@@ -29,4 +40,8 @@ class Reserva{
         int obtenerEdad(int);
         string mostrarCorreo(string);
         string mostrarTelefono(string);
+        float calcularPrecioDeluxe(float, bool, bool, string);
+        float calcularPrecioSimple(float, bool);
+        double numHabitacionDeluxe();
+        double numHabitacionSimple();
 };
