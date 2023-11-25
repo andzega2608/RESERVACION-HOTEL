ced5b4318f38d416a79c4ddc79cc715381030508
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:50:19 -0600

    Update Simple.cpp

--- Simple.cpp
+++ Simple.cpp
@@ -1,11 +1,15 @@
 #include "Simple.h"
+// Constructor vacio
 Simple::Simple(){
 }
-
-Simple::Simple(float precio, bool _hidromasaje) : Habitacion(precio){
+// Constructor para establecer precio e hidromasaje
+Simple::Simple(float precio, bool _hidromasaje) : Habitacion(precio){ // Se implementa herencia
     hidromasaje = _hidromasaje;
 }
-
+void Simple::setHidromasaje(bool _hidromasaje){
+    hidromasaje = _hidromasaje;
+}
+// Método que sirve paraa calcular el precio de la habitacion
 float Simple::calcularPrecio(){
     float aumento = 0.0;
     if (hidromasaje == true){
