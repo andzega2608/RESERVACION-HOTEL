665d78fbf515ef915013d0cff5d5a0f00b992a56
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:43:55 -0600

    Update Deluxe.h

--- Deluxe.h
+++ Deluxe.h
@@ -1,7 +1,10 @@
+/*La subclase Deluxe hace referencia a uno de los dos tipos de habitaciones que el hotel dispone para sus huespedes.
+La habitación es sinonimo de lujo y exclusividad. Pues, puede ser personalizada dependiendo los gustos del cliente como ser:
+incluir jacuzzi, vista al mar y el plan de comidas como ser de tipo buffet.*/
 #include "Habitacion.h"
 #include <string>
 
-class Deluxe : public Habitacion {
+class Deluxe : public Habitacion {  //Se implementa herencia
 private:
     bool jacuzzi;
     bool vistaMar;
