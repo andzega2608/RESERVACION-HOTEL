182dae1a223a2e29c093923c932f3c63f06bf66e
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:45:37 -0600

    Update Habitacion.h

--- Habitacion.h
+++ Habitacion.h
@@ -1,8 +1,14 @@
+/*La clase Habitacion es la clase padre de las clases Deluxe y Simple. Dicha clase tiene como fin heredar los atributos
+precio de cada habitación, así también los métodos.
+*/
+#ifndef HABITACION_H // Sirve para definir la clase y no generar un error de redefinición de la clase.
+#define HABITACION_H
 class Habitacion {
-    private:
+    //Atributos de la clase. Protegidos porque se implementa herencia
+    protected:
         float precio;
         int numHabitacion;
-
+    //Métodos de la clase
     public:
         Habitacion();
         Habitacion(float _precio);
@@ -10,3 +16,4 @@ class Habitacion {
         float getPrecio();
         int getNumHabitacion();
 };
+#endif
