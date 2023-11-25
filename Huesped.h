8ca0bb0bb5ab109e12f2238a3a62d1e3909e4848
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2023-11-25 16:46:52 -0600

    Update Huesped.h

--- Huesped.h
+++ Huesped.h
@@ -1,3 +1,6 @@
+/*La clase Huesped, es una clase de composición de la clase Reserva. Mediante esta clase, se pueden ingresar los datos
+de los huespedes que ocuparan las habitaciones, los datos que se piden son: nombre, apellido, nacionalidad y edad.
+*/
 #include <string>
 using namespace std;
 class Huesped {
